b7ed31b6d62fe0244467d05bed06eb8a77464ffa
Jasmeh Singh <142116700+jasmehsidhu@users.noreply.github.com>
2025-09-02 16:09:10 -0400

    Add files via upload

---
+++ backend/script.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors'
+import compression from 'compression';
+import pg from 'pg'
+import bcrypt from 'bcrypt'
+
+const app=express();
+
+
+app.listen(2000,()=>{
+    console.log("Server Started!")
+})
+app.use(express.json())
+app.use(cors())
+app.use(compression())
+var db=new pg.Client({
+    database:'TFSS_Events',
+    user:'postgres',
+    host:'localhost',
+    port:5432,
+    password:'sidhumoosewala'
+})
+db.connect()
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    db.query(`INSERT INTO Events VALUES('${req.body.name}','${req.body.discription}','${req.body.date}','${req.body.type}')`,()=>{
+        res.send('done')
+    })
+})
+app.get('/',(req,res)=>{
+    db.query('SELECT * FROM Events',(err,rows)=>{
+        var arr=rows.rows;
+        arr.reverse()
+res.json(arr)    })
+})
+app.post('/submit',(req,res)=>{
+
+    db.query(`SELECT * FROM auths WHERE username='${req.body.username}'`,(err,rows)=>{
+    if(rows.rows.length==0||err){
+        res.send(false)
+    }
+    else{
+        bcrypt.compare(req.body.password,rows.rows[0].password,(err,result)=>{
+       res.send(result)
+    })
+    }
+    
+})
+
+})
